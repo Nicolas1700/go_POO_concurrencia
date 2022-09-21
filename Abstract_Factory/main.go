1a54e6471732764c2ee50fa51c13cb69c4e0b922
Nicolas <nicolas.pc.1700@gmail.com>
2022-09-21 17:51:07 -0500

    Se termina el abstract_factory/main.go, el cual se incluye el mail sus metodos y funciones de uso ante el uso de un packgate main

--- Abstract_Factory/main.go
+++ Abstract_Factory/main.go
@@ -16,11 +16,13 @@ type IEnviar interface {
 }
 // Fin interfaces
 
+// SMS
 // Struct SMS notificacion 
 type SMSNotification struct {
 
 }
 
+// Met Enviar Mensaje
 func (SMSNotification) EnviarMensaje() IEnviar{
 	// Se devuleve el STRUCT 
 	return SMSEnviarNotificacion{}
@@ -28,7 +30,7 @@ func (SMSNotification) EnviarMensaje() IEnviar{
 
 // MetS EnviarNotificacion de SMS
 func (SMSNotification) EnviarNotificacion() {
-	fmt.Println("El mensaje se esta enviando")
+	fmt.Println("El mensaje se esta enviando por SMS")
 }
 
 
@@ -46,9 +48,77 @@ func (SMSEnviarNotificacion) CanalDeEnvio() string {
 	return "TWilio"
 }
 // Fin metodos -> SMSEnviarNotificacion
+//Fin SMS
 
+
+
+// EMAIL
+// Struct SMS notificacion 
+type EmailNotification struct {
+
+}
+
+// Met Enviar Mensaje -> EmailNotification
+func (EmailNotification) EnviarMensaje() IEnviar{
+	// Se devuleve el STRUCT 
+	return EmailEnviarNotificacion{}
+}
+
+// MetS EnviarNotificacion de SMS -> EmailNotification
+func (EmailNotification) EnviarNotificacion() {
+	fmt.Println("El mensaje se esta enviando por email")
+}
+
+// Struct Enviar notificaciones
+type EmailEnviarNotificacion struct {
+
+}
+
+// Metodos -> EmailEnviarNotificacion
+// Met enviar mensaje -> EmailEnviarNotificacion
+func (EmailEnviarNotificacion) MetodoEnviarMensage() string {
+	return "Email"
+}
+// Met canal de envio -> EmailEnviarNotificacion
+func (EmailEnviarNotificacion) CanalDeEnvio() string {
+	return "SES" // Servicio de Amazon
+}
+// Fin metodos -> EmailEnviarNotificacion
+// Fin EMAIL
+
+
+
+// Func para conocer el tipo de notificacion
+func ObtenerNotificacion (notificacion string) (IFabricaNotificaciones, error) {
+	if notificacion == "SMS" {
+		return &SMSNotification{}, nil
+	}
+	if notificacion == "Email" {
+		return &EmailNotification{}, nil
+	}
+	return nil, fmt.Errorf("No se genero la notificación")
+}
+
+// func para realizar el envio de la notificiacion 
+func enviarNotificacion(f IFabricaNotificaciones){
+	f.EnviarNotificacion()
+}
+
+// func para obtener el metodo usado
+func obtenerMetodo(f IFabricaNotificaciones){
+	fmt.Println(f.EnviarMensaje().MetodoEnviarMensage()) 
+}
 
 
 func main () {
+
+	smsFactory, _ := ObtenerNotificacion("SMS")
+	emailFactory, _ := ObtenerNotificacion("Email")
+
+	enviarNotificacion(smsFactory)
+	enviarNotificacion(emailFactory)
+
+	obtenerMetodo(smsFactory)
+	obtenerMetodo(emailFactory)
 
 }
